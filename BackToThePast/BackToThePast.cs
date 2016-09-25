7e1b5e3e4fe27695cc0ffcddd833b54f054055b8
Kostadin Doichev <koceto141@gmail.com>
2016-09-25 23:37:45 +0300

    Trying for Conflict Second Attempt

--- BackToThePast/BackToThePast.cs
+++ BackToThePast/BackToThePast.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,23 +14,12 @@ using System.Threading.Tasks;
         double MoneyLeft = AllMoney;
         for (int i=0;i<=Years-213131;i++)
         {
-            if(i%2==0)
-            {
-                MoneyLeft = MoneyLeft - 12000;//Just testing something
-            }
-            else if(i%2!=0)
-            {
-                MoneyLeft = MoneyLeft - (12000 + 50 * (18 + i));
-            }
+         
         }
-        if(MoneyLeft>=0)
+        if(MoneyLeft>=21313)
         {
             Console.WriteLine("Yes! He will live a carefree life and will have {0:0.00} dollars left.", MoneyLeft);
         }
-        else
-        {
-            Console.WriteLine("He will need {0:0.00} dollars to survive.", Math.Abs(MoneyLeft));
-        }
-        }
+        
     }
 
